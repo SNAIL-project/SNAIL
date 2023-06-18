a50cd001b8a7e912bc171991426aa43e394f4990
JulianALZ <julian.alizay@gmail.com>
2023-06-18 16:08:30 +0200

    fix reward function + start looking PPO

--- main.py
+++ main.py
@@ -3,53 +3,45 @@ import numpy as np
 
 import rlgym
 from rlgym.envs import Match
-from rlgym.utils.reward_functions.common_rewards.misc_rewards import EventReward
-from rlgym.utils.reward_functions.common_rewards.player_ball_rewards import VelocityPlayerToBallReward
-from rlgym.utils.reward_functions.common_rewards.ball_goal_rewards import VelocityBallToGoalReward
-from rlgym.utils.reward_functions import CombinedReward
 from rlgym.utils.terminal_conditions.common_conditions import TimeoutCondition
 from rlgym.utils.obs_builders import AdvancedObs
-from rlgym.utils.state_setters import DefaultState
 from rlgym.utils.action_parsers import DiscreteAction
 from stable_baselines3.common.vec_env import VecMonitor
 
 from rlgym_tools.sb3_utils import SB3MultipleInstanceEnv
-from stable_baselines3.common.callbacks import CheckpointCallback
-
 from stable_baselines3 import PPO
 
 from game_state import CustomStateSetterExo1, CustomStateSetterExo2, CustomStateSetterExo3
 from terminal_conditions import BallTouchGround
-from Rewards import PlayerUnderBall
-from Rewards import  PlayerUnderBall2
+from Rewards import PlayerUnderBall2
 
 # =============================== option for model ============================================
 agents_per_match = 1
 num_instances = 1
-target_steps = 100_000
+target_steps = 1000
 steps = target_steps // (num_instances * agents_per_match)
 batch_size = target_steps // 10
 mmr_save_frequency = 200_000
 
 # ==================================== EXO ==============================================
-exo = 2
+exo = 1
 if exo == 1:
-    print("Exo 1 launch")
     instance_state = CustomStateSetterExo1()
-    tensorboard_log: str = "logs_exo1" #f"logs_exo{exo}"
+    tensorboard_log: str = f"logs_exo{exo}"
     path_model_save: str = "models/PPO_model_exo1.zip"
+    print("Exo 1 launch")
 
 elif exo == 2:
-    print("Exo 2 launch")
     instance_state = CustomStateSetterExo2()
     tensorboard_log: str = f"logs_exo{exo}"
     path_model_save: str = "models/PPO_model_exo2.zip"
+    print("Exo 2 launch")
 
 elif exo == 3:
-    print("Exo 3 launch")
     instance_state = CustomStateSetterExo3()
     tensorboard_log: str = f"logs_exo{exo}"
     path_model_save: str = "models/PPO_model_exo3.zip"
+    print("Exo 3 launch")
 else:
     sys.exit("Veuillez sélectionner un exo.")
 
@@ -71,9 +63,7 @@ def get_match():
 # ==================================================================================
 
 if __name__ == "__main__":
-    print("test 0")
     env = SB3MultipleInstanceEnv(get_match, num_instances, wait_time=20)
-    print("test 0.5")
     env = VecMonitor(env)
 
     try:
@@ -97,13 +87,22 @@ if __name__ == "__main__":
             batch_size=batch_size,  # Batch size as high as possible within reason
             n_steps=steps,  # Number of steps to perform before optimizing network
             tensorboard_log=tensorboard_log,  # `tensorboard --logdir logs_exoXX/PPO_0` in terminal to see graphs
-            device="auto"  # Uses GPU if available
+            device="auto",  # Uses GPU if available
+
+            #ent_coef = 0.1 # C'est le coefficient pour la pénalité d'entropie. Cela encourage l'exploration en ajoutant une pénalité pour les politiques de faible entropie
+                            # (c'est-à-dire les politiques trop certaines). Si vous constatez que votre agent n'explore pas assez, augmenter cette valeur pourrait aider.
+
+            # Utiliser une autre politique: Vous utilisez "MlpPolicy" qui est une politique à base de perceptron multicouche. Vous pourriez envisager d'essayer d'autres politiques,
+                            #  comme "CnnPolicy" si votre environnement a des entrées basées sur des images, ou "LstmPolicy" si votre environnement a des dépendances temporelles.
+
+
+
         )
 
     try:
         mmr_model_target_count = model.num_timesteps + mmr_save_frequency
         while True:
-            model.learn(target_steps, reset_num_timesteps=False)  # TODO fix problem de save logs
+            model.learn(target_steps, reset_num_timesteps=False)
             model.save(path_model_save)
             if model.num_timesteps >= mmr_model_target_count:
                 model.save(f"mmr_models/{model.num_timesteps}")
