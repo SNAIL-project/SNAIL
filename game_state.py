cd52a8b9b190a83a22514941d45351c1c4e40def
JulianALZ <julian.alizay@gmail.com>
2023-07-06 19:42:26 +0200

    add exos 4.5.6

--- game_state.py
+++ game_state.py
@@ -1,6 +1,6 @@
-import numpy as np
 from rlgym.utils.state_setters import StateSetter
 from rlgym.utils.state_setters import StateWrapper
+import math
 
 
 class CustomStateSetterExo1(StateSetter):
@@ -74,5 +74,90 @@ class CustomStateSetterExo3(StateSetter):
         state_wrapper.ball.set_pos(x=-400, y=0,
                                    z=800)  # height_ball=153.0 mais le rayon de la balle est 93.0u  & hitbox_car = 36 so ball spawn at 189u & #max height 2044
         state_wrapper.ball.set_lin_vel(200, 0, 0)  # la vitesse de la balle dans les trois dimensions de l'espace)
+        state_wrapper.ball.set_ang_vel(0, 0,
+                                       0)  # la vitesse de rotation de la balle autour de chacun de ses axes (en roulis, tangage et lacet)
+
+
+class CustomStateSetterExo4(StateSetter):
+    def __init__(self):
+        super().__init__()
+
+    def reset(self, state_wrapper: StateWrapper):
+        for car in state_wrapper.cars:
+            if car.team_num == 0:
+                desired_car_pos = [0, 0, 17]  # x, y, z
+                yaw = 0
+            elif car.team_num == 1:
+                desired_car_pos = [0, 0, 17]  # x, y, z
+                yaw = 0
+
+        car.set_pos(*desired_car_pos)
+        car.set_rot(yaw=yaw)
+        car.boost = 0
+
+        # Now we will spawn the ball in the center of the field, floating in the air.
+        state_wrapper.ball.set_pos(x=-4000, y=0,
+                                   z=1500)  # height_ball=153.0 mais le rayon de la balle est 93.0u  & hitbox_car = 36 so ball spawn at 189u & #max height 2044
+
+        state_wrapper.ball.set_lin_vel(4000, 0, 0)  # la vitesse de la balle dans les trois dimensions de l'espace)
+        state_wrapper.ball.set_ang_vel(0, 0, 0)  # la vitesse de rotation de la balle autour de chacun de ses axes (en roulis, tangage et lacet)
+
+
+class CustomStateSetterExo5(StateSetter):
+    def __init__(self):
+        super().__init__()
+
+    def limit_to_radius(self, x, y, radius):
+        distance = math.sqrt(x ** 2 + y ** 2)
+        if distance > radius:
+            angle = math.atan2(y, x)
+            x = math.cos(angle) * radius
+            y = math.sin(angle) * radius
+        return x, y
+
+    def reset(self, state_wrapper: StateWrapper):
+        for car in state_wrapper.cars:
+            if car.team_num == 0:
+                desired_car_pos = [0, 0, 17]  # x, y, z
+                yaw = 0
+            elif car.team_num == 1:
+                desired_car_pos = [0, 0, 17]  # x, y, z
+                yaw = 0
+
+        car.set_pos(*desired_car_pos)
+        car.set_rot(yaw=yaw)
+        car.boost = 0
+
+        radius = 500
+        x, y, z = -400, 0, 800
+        x, y = self.limit_to_radius(x, y, radius)
+
+        # Now we will spawn the ball in the center of the field, floating in the air.
+        state_wrapper.ball.set_pos(x=x, y=y, z=z)
+        state_wrapper.ball.set_lin_vel(400, 0, 0)
+        state_wrapper.ball.set_ang_vel(0, 0, 0)  # la vitesse de rotation de la balle autour de chacun de ses axes (en roulis, tangage et lacet)
+
+
+class CustomStateSetterExo6(StateSetter):
+    def __init__(self):
+        super().__init__()
+
+    def reset(self, state_wrapper: StateWrapper):
+        for car in state_wrapper.cars:
+            if car.team_num == 0:
+                desired_car_pos = [0, 0, 17]  # x, y, z
+                yaw = 0
+            elif car.team_num == 1:
+                desired_car_pos = [0, 0, 17]  # x, y, z
+                yaw = 0
+
+        car.set_pos(*desired_car_pos)
+        car.set_rot(yaw=yaw)
+        car.boost = 0
+
+        # Now we will spawn the ball in the center of the field, floating in the air.
+        state_wrapper.ball.set_pos(x=-3200, y=-4000,
+                                   z=400)  # height_ball=153.0 mais le rayon de la balle est 93.0u  & hitbox_car = 36 so ball spawn at 189u & #max height 2044
+        state_wrapper.ball.set_lin_vel(2000, 2000, 2000)  # la vitesse de la balle dans les trois dimensions de l'espace)
         state_wrapper.ball.set_ang_vel(0, 0,
                                        0)  # la vitesse de rotation de la balle autour de chacun de ses axes (en roulis, tangage et lacet)
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from stable_baselines3.common.vec_env import VecMonitor
 from rlgym_tools.sb3_utils import SB3MultipleInstanceEnv
 from stable_baselines3 import PPO
 
-from game_state import CustomStateSetterExo1, CustomStateSetterExo2, CustomStateSetterExo3
+from game_state import CustomStateSetterExo1, CustomStateSetterExo2, CustomStateSetterExo3, CustomStateSetterExo4, CustomStateSetterExo5, CustomStateSetterExo6
 from terminal_conditions import BallTouchGround
 from Rewards import PlayerUnderBall2
 
@@ -24,7 +24,7 @@ batch_size = target_steps // 10
 mmr_save_frequency = 200_000
 
 # ==================================== EXO ==============================================
-exo = 1
+exo = 6
 if exo == 1:
     instance_state = CustomStateSetterExo1()
     tensorboard_log: str = f"logs_exo{exo}"
@@ -42,6 +42,21 @@ elif exo == 3:
     tensorboard_log: str = f"logs_exo{exo}"
     path_model_save: str = "models/PPO_model_exo3.zip"
     print("Exo 3 launch")
+elif exo == 4:
+    instance_state = CustomStateSetterExo4()
+    tensorboard_log: str = f"logs_exo{exo}"
+    path_model_save: str = "models/PPO_model_exo4.zip"
+    print("Exo 4 launch")
+elif exo == 5:
+    instance_state = CustomStateSetterExo5()
+    tensorboard_log: str = f"logs_exo{exo}"
+    path_model_save: str = "models/PPO_model_exo5.zip"
+    print("Exo 5 launch")
+elif exo == 6:
+    instance_state = CustomStateSetterExo6()
+    tensorboard_log: str = f"logs_exo{exo}"
+    path_model_save: str = "models/PPO_model_exo6.zip"
+    print("Exo 6 launch")
 else:
     sys.exit("Veuillez sélectionner un exo.")
 
