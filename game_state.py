b126e95e9f11ce3248b017d92dced76ebd3ce439
JulianALZ <90183150+JulianALZ@users.noreply.github.com>
2023-07-23 15:06:55 +0200

    Update game_state.py, delete old exo5 + comments in exo6

--- game_state.py
+++ game_state.py
@@ -126,40 +126,6 @@ class CustomStateSetterExo5(StateSetter):
         state_wrapper.ball.set_ang_vel(0, 0, 0)
 
 
-# class CustomStateSetterExo5(StateSetter):
-#     def __init__(self):
-#         super().__init__()
-#
-#     def limit_to_radius(self, x, y, radius):
-#         distance = math.sqrt(x ** 2 + y ** 2)
-#         if distance > radius:
-#             angle = math.atan2(y, x)
-#             x = math.cos(angle) * radius
-#             y = math.sin(angle) * radius
-#         return x, y
-#
-#     def reset(self, state_wrapper: StateWrapper):
-#         for car in state_wrapper.cars:
-#             if car.team_num == 0:
-#                 desired_car_pos = [0, 0, 17]  # x, y, z
-#                 yaw = 0
-#             elif car.team_num == 1:
-#                 desired_car_pos = [0, 0, 17]  # x, y, z
-#                 yaw = 0
-#
-#         car.set_pos(*desired_car_pos)
-#         car.set_rot(yaw=yaw)
-#         car.boost = 0
-#
-#         radius = 1000
-#         x, y, z = np.random.randint(-radius,radius), np.random.randint(-radius,radius), 1000
-#         x, y = self.limit_to_radius(x, y, radius)
-#
-#         state_wrapper.ball.set_pos(x=x, y=y, z=z)
-#         state_wrapper.ball.set_lin_vel(np.random.randint(-400,400), np.random.randint(-200,200), 0)
-#         state_wrapper.ball.set_ang_vel(0, 0, 0)
-
-
 class CustomStateSetterExo6(StateSetter):
     def __init__(self):
         super().__init__()
@@ -177,9 +143,8 @@ class CustomStateSetterExo6(StateSetter):
         car.set_rot(yaw=yaw)
         car.boost = 0
 
-        # Now we will spawn the ball in the center of the field, floating in the air.
         state_wrapper.ball.set_pos(x=-3200, y=-4000,
-                                   z=400)  # height_ball=153.0 mais le rayon de la balle est 93.0u  & hitbox_car = 36 so ball spawn at 189u & #max height 2044
-        state_wrapper.ball.set_lin_vel(2000, 2000, 2000)  # la vitesse de la balle dans les trois dimensions de l'espace)
+                                   z=400)
+        state_wrapper.ball.set_lin_vel(2000, 2000, 2000)
         state_wrapper.ball.set_ang_vel(0, 0,
-                                       0)  # la vitesse de rotation de la balle autour de chacun de ses axes (en roulis, tangage et lacet)
+                                       0)
