ec0e0d9305e74e36b8539c6e202e45a290962d85
JulianALZ <90183150+JulianALZ@users.noreply.github.com>
2023-07-23 20:09:59 +0200

    Update terminal_conditions.py

--- terminal_conditions.py
+++ terminal_conditions.py
@@ -9,12 +9,11 @@ class BallTouchGround(TerminalCondition):
     def __init__(self):
         super().__init__()
 
-    def reset(self, initial_state: GameState):  # Called once per reset.
+    def reset(self, initial_state: GameState):
         pass
 
-    def is_terminal(self, current_state: GameState) -> bool:  # Called once per step.
+    def is_terminal(self, current_state: GameState) -> bool:
         """
         Return `True` if the ball hit the gound
         """
-        # print(current_state.ball.position)
-        return current_state.ball.position[2] <= 103  # bcs it s the height of the top of the ball (94u) but the game is BUGED? so we put 103u
+        return current_state.ball.position[2] <= 103 
